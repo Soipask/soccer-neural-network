a145fe5bd668108e9f04e07a268abfdcf22027c7
Soipask <38126547+Soipask@users.noreply.github.com>
2018-09-06 10:29:49 +0200

    First commit

---
+++ PointsCounting/PointsCounting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileHelpers;
+
+namespace PointsCounting
+{
+    [DelimitedRecord(",")]
+    class Match
+    {
+        public string homeTeam;
+        public string awayTeam;
+        public int id;
+        public int homeTeamId;
+        public int awayTeamId;
+        public int homeTeamGoalsScored;
+        public int awayTeamGoalsScored;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new FileHelperEngine<Match>();
+
+            var result = engine.ReadFile("../../../../Databases/england08.csv");
+        }
+
+
+    }
+}
