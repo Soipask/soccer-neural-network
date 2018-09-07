4fa08ace40cce7a00dac02d98d8169f2d7b72124
Soipask <38126547+Soipask@users.noreply.github.com>
2018-09-07 16:33:06 +0200

    Added form - remembering results from up to 10 last games along with team they played.

--- PointsCounting/PointsCounting/Program.cs
+++ PointsCounting/PointsCounting/Program.cs
@@ -18,6 +18,7 @@ namespace PointsCounting
         public int awayTeamId;
         public int homeTeamGoalsScored;
         public int awayTeamGoalsScored;
+        public string date;
     }
 
     class Team
@@ -27,6 +28,7 @@ namespace PointsCounting
         public int goalsFor;
         public int goalsAgainst;
         public int points;
+        public MatchForm[] currentForm = new MatchForm[10];
 
         public Team(string name, int id, int gf, int ga, int p)
         {
@@ -39,7 +41,30 @@ namespace PointsCounting
 
         public override string ToString()
         {
-            return shortName + "\t" + points + "\t" + goalsFor + "\t" + goalsAgainst + "\t" + (goalsFor-goalsAgainst);
+            string str = shortName + "\t" + points + "\t" + goalsFor + "\t" + goalsAgainst + "\t" + (goalsFor - goalsAgainst) + "\t";
+            for (int i = 0; i < currentForm.Length; i++) 
+            {
+                if (currentForm[i] == null) break;
+                str += currentForm[i];
+            }
+            return str;
+        }
+    }
+
+    class MatchForm
+    {
+        public int finalpoints;
+        public Team matchAgainst;
+
+        public override string ToString()
+        {
+            switch (finalpoints)
+            {
+                case 0: return "L";
+                case 1: return "D";
+                case 3: return "W";
+                default: return "";
+            }
         }
     }
 
@@ -60,7 +85,10 @@ namespace PointsCounting
 
             teamDictionary.Add(match.homeTeamId,homeTeam);
             teamDictionary.Add(match.awayTeamId,awayTeam);
-        
+
+            ChangeInForm(match, homeTeam, homePoints);
+            ChangeInForm(match, awayTeam, awayPoints);
+
         }
         private static void AddPoints(Match match)
         {
@@ -72,12 +100,13 @@ namespace PointsCounting
             homeTeam.points += homePoints;
             homeTeam.goalsFor += match.homeTeamGoalsScored;
             homeTeam.goalsAgainst += match.awayTeamGoalsScored;
+            ChangeInForm(match, homeTeam, homePoints);
 
             awayTeam.points += awayPoints;
             awayTeam.goalsFor += match.awayTeamGoalsScored;
             awayTeam.goalsAgainst += match.homeTeamGoalsScored;
+            ChangeInForm(match, awayTeam, awayPoints);
         }
-
         private static void GetPoints(Match match,out int homePoints, out int awayPoints)
         {
             if (match.homeTeamGoalsScored > match.awayTeamGoalsScored)
@@ -94,6 +123,28 @@ namespace PointsCounting
             {
                 homePoints = awayPoints = 1;
             }
+        }
+        private static void ChangeInForm(Match match, Team team, int points)
+        {
+            MatchForm form = new MatchForm { finalpoints = points };
+
+            Team matchAgainst;
+
+            if (team.id == match.homeTeamId)
+            {
+                teamDictionary.TryGetValue(match.awayTeamId, out matchAgainst);
+            }
+            else
+            {
+                teamDictionary.TryGetValue(match.homeTeamId, out matchAgainst);
+            }
+
+            for (int i = team.currentForm.Length - 1; i > 0; i--) 
+            {
+                team.currentForm[i] = team.currentForm[i - 1];
+            }
+
+            team.currentForm[0] = form;
         }
 
         static void Main(string[] args)
@@ -120,9 +171,9 @@ namespace PointsCounting
                 }
             }
 
-            var endOfSeason = table.OrderByDescending(x => x.goalsFor - x.goalsAgainst).OrderByDescending(x => x.points);
+            var endOfSeason = table.OrderByDescending(x => x.points).ThenByDescending(x => x.goalsFor - x.goalsAgainst);
             
-            Console.WriteLine("TEA \t POI \t GF \t GA \t DIFF");
+            Console.WriteLine("TEA \t POI \t GF \t GA \t DIFF \t FORM");
             foreach(var team in endOfSeason)
             {
                 Console.WriteLine(team);
